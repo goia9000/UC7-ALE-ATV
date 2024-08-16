a46b259d34b1e452f1a5a641cf06beb75e3d7a60
Bruno Goia <brunogoia92@gmail.com>
2024-08-16 15:32:51 -0300

    Correção de estilos repetidos

--- Atividade4/processamento.php
+++ Atividade4/processamento.php
@@ -3,10 +3,9 @@
 $num = $_GET["num"];
 $mensagem = "";
 
-if ($num <=0) {
-    $mensagem = "O número ". $num ." não é válido";
-}
-else if ($num % 2 === 0) {
+if ($num <= 0) {
+    $mensagem = "O número " . $num . " não é válido";
+} else if ($num % 2 === 0) {
     $mensagem = "O número $num é par!";
 } else {
     $mensagem = "O número $num é impar!";
